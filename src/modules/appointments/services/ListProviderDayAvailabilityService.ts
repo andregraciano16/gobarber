62752b504e107f9ab1c7fea7aa6b01d481ea1385
André Graciano <andregraciano16@gmail.com>
2021-01-31 22:25:29 -0300

    validação para não agendar em data anterior

--- src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getDaysInMonth, getDate, getHours } from 'date-fns';
+import { getDaysInMonth, getDate, getHours, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -40,13 +40,15 @@ class ListProviderDayAvailabilityService {
             (_, index) => index + hourStart,
         );
 
+       const currenceDate = new Date(Date.now());
         const availability = eachDays.map(hour => {
             const appointmentsHour = appointments.find(appointment => {
                 return getHours(appointment.date) === hour;
             });
+            const compareDate = new Date(year, month -1, day, hour);
             return {
                 hour,
-                available: !appointmentsHour,
+                available: !appointmentsHour && isAfter(compareDate, currenceDate),
             };
         });
 
