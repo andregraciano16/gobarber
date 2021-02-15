da320b0b18636ad2235c684fac83da2dd4d222f0
André Graciano <andregraciano16@gmail.com>
2021-02-14 21:01:01 -0300

    adicionando classe transformer

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,22 +1,22 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
+
 import AuthenticateUserService from '../../../services/AuthenticateUserService';
 
 export default class SessionsController {
     public async create(request: Request, response: Response): Promise<Response> {
-        console.log('->> Método de login')
 
         const { email, password } = request.body;
 
         const authenticateUser = container.resolve(AuthenticateUserService);
-        console.log('-> Validando dados do usuário');
+
         const { user, token } = await authenticateUser.execute({
             email,
             password,
         });
 
-        delete user.password;
         console.log('-> usuário validado com sucesso');
-        return response.json({ user, token });
+        return response.json({ user: classToClass(user), token });
     }
 }
